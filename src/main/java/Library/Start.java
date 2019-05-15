a96e04ca8c3c9577d76776e238d6815d6771c90e
AnetaGolebiowska <aneta.golebiowskaa@gmail.com>
2019-05-16 00:25:37 +0200

    Commit3

---
+++ src/main/java/Library/Start.java
@@ -0,0 +1,14 @@
+package Library;
+
+public class Start {
+
+    public void startMenu (){
+        System.out.println("Wybierz opcję: " +
+                "\n 1. Lista książek " +
+                "\n 2. Wyjdź " +
+                "\n 3. Dodaj książkę " +
+                "\n 4. Usuń książkę " +
+                "\n 5. Edycja roku wydania książki " +
+                "\n 6. Zapisz listę książek w csv");
+    }
+}
