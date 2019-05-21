8001d50ea440efd6a8f42571f9fc687db6ec14fe
AnetaGolebiowska <aneta.golebiowskaa@gmail.com>
2019-05-21 20:52:34 +0200

    Excercises 18-19

--- src/main/java/library/Book.java
+++ src/main/java/library/Book.java
@@ -1,18 +1,36 @@
 package library;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class Book {
+
+    private List<Authors> authorsList;
+    private Categorie categorie;
     private String title;
     private int isbnNumber;
     private int year;
-//    List <> authors = new ArrayList<>();
 
-    public Book(String title, int isbnNumner, int year) {
+    public Book(List<Authors> authorsList, Categorie categorie, String title, int isbnNumner, int year) {
         this.title = title;
         this.isbnNumber = isbnNumber;
         this.year = year;
+        this.authorsList = authorsList;
+        this.categorie = categorie;
+    }
+
+    public List<Authors> getAuthorsList() {
+        return authorsList;
+    }
+
+    public void setAuthorsList(List<Authors> authorsList) {
+        this.authorsList = authorsList;
+    }
+    public Categorie getCategorie() {
+        return categorie;
+    }
+
+    public void setCategorie(Categorie categorie) {
+        this.categorie = categorie;
     }
 
     public String getTitle() {
@@ -22,6 +40,7 @@ public class Book {
     public void setTitle(String title) {
         this.title = title;
     }
+
 
     public int getIsbnNumber() {
         return isbnNumber;
@@ -38,6 +57,8 @@ public class Book {
     public void setYear(int year) {
         this.year = year;
     }
+
+
 
     @Override
     public String toString() {
--- src/main/java/library/BookLoader.java
+++ src/main/java/library/BookLoader.java
@@ -11,25 +11,41 @@ import java.util.stream.Stream;
 public class BookLoader {
 
     List<Book> listOfBooks = new ArrayList<Book>();
+    List<Authors> listOfAuthors = new ArrayList<Authors>();
+    List<Categorie> listOfCategories = new ArrayList<Categorie>();
+
 
     public void fileLoadBook() {
-        try (Stream<String> stream = Files.lines(Paths.get("src/Main/resources/books.csv"))) {
-            listOfBooks = stream.map(s -> s.split(";"))
-                    .map(a -> new Book(a[0], Integer.valueOf(a[1]), Integer.valueOf(a[2])))
+        Stream<String> stream = null;
+        try {
+            stream = Files.lines(Paths.get("src/Main/resources/authors.csv"));
+            listOfAuthors = stream.map(s -> s.split(";"))
+                    .map(a -> new Authors(Integer.valueOf(a[0]), a[1], Integer.valueOf(a[2])))
                     .collect(Collectors.toList());
         } catch (IOException e) {
             System.out.println("Nie udało się załadować książek z pliku");
         }
-    }
 
-//    public void fileLoadAuthors() {
-//        try (Stream<String> stream = Files.lines(Paths.get("src/Main/resources/authors.csv"))) {
-//            listOfAuthots = stream.map(s -> s.split(";"))
-//                    .map(a -> new Authors(Integer.valueOf(a[0]), a[1], Integer.valueOf(a[2])))
+
+        try {
+            Stream<String> stream2 = Files.lines(Paths.get("src/Main/resources/categories.csv"));
+            listOfCategories = stream.map(s -> s.split(";"))
+                    .map(a -> new Categorie(Integer.valueOf(a[0]), a[1], Integer.valueOf(a[2])))
+                    .collect(Collectors.toList());
+        } catch (IOException e) {
+            System.out.println("Nie udało się załadować książek z pliku");
+        }
+
+
+//        Stream<String> stream3 = Files.lines(Paths.get("src/Main/resources/books.csv"));
+
+//            listOfBooks = stream.map(s -> s.split(";"))
+//                    .map(a -> new Book(a[0], Integer.valueOf(a[1]), Integer.valueOf(a[2])))
 //                    .collect(Collectors.toList());
-//        } catch (IOException e) {
-//            System.out.println(("Nie udało się załadować książek z pliku"));
+//
+//            System.out.println("Nie udało się załadować książek z pliku");
 //        }
-//    }
-
+    }
 }
+
+
